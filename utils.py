b419b20ac7a2f4ff8b4546e06b125d6f567b3bb8
Yubraj Upadhaya <gyubraj104@gmail.com>
2022-06-15 10:29:39 +0545

    Add email and password validation

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """
 Some simple utils functions which are used many times 
 """
+import re
 
 
 def print_star() -> None:
@@ -23,6 +24,20 @@ def input_user_details(type: str) -> tuple[str]:
     """
     )
 
+    email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+
     email = input("\t\t\tEmail Address :  ")
-    password = input("\t\t\tPassword :  ")
+    while not re.fullmatch(email_pattern, email):
+        print("\n\n\t\t\tPlease Enter Valid Email Address.\n\n")
+        email = input("\t\t\tEmail Address :  ")
+
+    password = input("\t\t\tPassword :  ").strip()
+    password_pattern = (
+        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,14}$"
+    )
+    while not re.fullmatch(password_pattern, password):
+        print(
+            "\n\n\t\t\tPassword should be between 8-14 chracter and should contain atleast 1 Captital letter, Number and Special Characters.\n\n"
+        )
+        password = input("\t\t\tPassword :  ").strip()
     return (email, password)
